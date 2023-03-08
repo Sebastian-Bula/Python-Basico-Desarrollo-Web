70d78e0b0c1634408d659f591ccc5a55220bb1a7
s3b4sbul4 <s3b4sbul4@gmmail.com>
2023-03-07 22:17:21 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+x = float(input('digita tu edad'))
+if x >= 21:
+    print('Eres mayor de edad en EEUU y en Colombia')
+elif x >= 18:
+    print('Eres mayor de edad en Colombia')
+else:
+    print('Eres menor de edad')
